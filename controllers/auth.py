5a6f4141265e68ddd7e71c6b6826d4ae498d864f
Perttu Heikkala <perttu.heikkala@edu.lapinamk.fi>
2024-12-07 13:05:39 +0200

    add route handlers get user by id, update user

--- controllers/auth.py
+++ controllers/auth.py
@@ -1,7 +1,7 @@
 from typing import List
 from fastapi import APIRouter
 from fastapi_pagination import Page, paginate
-from dtos.users import AddUserReqDto, UserDto
+from dtos.users import AddUserReqDto, UserDto, UpdateUserDto
 from mapper.mapper import ResponseMapper
 from services.service_factory import UserService
 
@@ -17,7 +17,17 @@ def get_all_users(service: UserService, mapper: ResponseMapper) -> List[UserDto]
     users = service.get_all()
     return paginate(mapper.map("user_dto", users))
 
+@router.get('/{user_id}')
+def get_user_by_id(user_id: int, service: UserService, mapper: ResponseMapper) -> UserDto:
+    user = service.get_by_id(user_id)
+    return mapper.map("user_dto", user)
+
 @router.post('/')
-def create_user(user_service: UserService, req: AddUserReqDto, mapper: ResponseMapper) -> UserDto:
-    user = user_service.create(req)
+def create_user(service: UserService, req: AddUserReqDto, mapper: ResponseMapper) -> UserDto:
+    user = service.create(req)
+    return mapper.map("user_dto", user)
+
+@router.put('/{user_id}')
+def update_user(user_id: int, service: UserService, mapper: ResponseMapper, req_data: UpdateUserDto) -> UserDto:
+    user = service.update_user(user_id, req_data)
     return mapper.map("user_dto", user)
