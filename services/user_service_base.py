a81f3267c95d77c956c3746a65922c330ff63d17
Perttu Heikkala <perttu.heikkala@edu.lapinamk.fi>
2024-12-07 13:09:21 +0200

    add abstractmethods get by id, update user, create user

--- services/user_service_base.py
+++ services/user_service_base.py
@@ -1,8 +1,21 @@
 import abc
+from dtos.users import UpdateUserDto, AddUserReqDto
 
 
 class UserServiceBase(abc.ABC):
 
     @abc.abstractmethod
     def get_all(self):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def get_by_id(self, user_id: int):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def update_user(self, user_id: int, req_data: UpdateUserDto):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def create(self, req: AddUserReqDto):
         raise NotImplementedError()
