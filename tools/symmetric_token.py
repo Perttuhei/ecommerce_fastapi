4b81200bfdcb2ca29f4bf0b55cac298cc92e8436
Perttu Heikkala <perttu.heikkala@edu.lapinamk.fi>
2024-12-07 15:28:46 +0200

    add validate_token method

--- tools/symmetric_token.py
+++ tools/symmetric_token.py
@@ -9,5 +9,7 @@ from tools.token_tool_base import TokenToolBase
 class SymmetricToken(TokenToolBase):
     def create_token(self, payload: [str, Any]) -> str:
         token = jwt.encode(payload, key=os.getenv("SECRET_KEY"), algorithm='HS512')
-
         return token
+
+    def validate_token(self, token_str) -> dict[str, Any]:
+        return jwt.decode(token_str, key=os.getenv("SECRET_KEY"), algorithms=['HS512'])
--- tools/token_tool_base.py
+++ tools/token_tool_base.py
@@ -8,3 +8,7 @@ class TokenToolBase(abc.ABC):
     @abc.abstractmethod
     def create_token(self, data: dict[str, Any]) -> str:
         raise NotImplementedError()
+
+    abc.abstractmethod
+    def validate_token(self, token_str) -> dict[str, Any]:
+        raise NotImplementedError()
